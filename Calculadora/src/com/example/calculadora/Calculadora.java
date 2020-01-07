4e95d2bcd07738dffa9001bb4eba8627996bf12f
cesarcamposc <camposcontreras.cesar@gmail.com>
2020-01-07 16:24:17 -0500

    Mi primer commit

---
+++ Calculadora/src/com/example/calculadora/Calculadora.java
@@ -0,0 +1,13 @@
+package com.example.calculadora;
+
+public class Calculadora {
+	public  int suma(int a, int b) {
+		return a + b;
+	}
+	
+	public  int resta(int a, int b) {
+		return a - b;
+	}
+
+
+}
---
+++ Calculadora/src/com/example/calculadora/CalculadoraTest.java
@@ -0,0 +1,19 @@
+package com.example.calculadora;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculadoraTest {
+
+	@Test
+	void testSuma() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testResta() {
+		fail("Not yet implemented");
+	}
+
+}
